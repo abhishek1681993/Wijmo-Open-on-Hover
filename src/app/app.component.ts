c08c49ccbcc5016bd467b71978c5e4d161f3fd6a
abhishek1681993 <ap1681993@gmail.com>
2019-12-27 12:38:42 +0530

    Wijmo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Inject } from '@angular/core';
+import { WjInputModule, WjMenu } from '@grapecity/wijmo.angular2.input';
+import { DataService } from './app.data';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  @ViewChild('theMenu') theMenu: WjMenu;
+    data: any[];
+    contextMenus = [];
+    //
+    constructor(@Inject(DataService) private dataService: DataService) {
+        this.data = dataService.getData();
+    }
+    //
+    showContextMenu(e: any) {
+        e.preventDefault();
+        setTimeout(() => {
+            this.contextMenus = [{label: 'L1', value: 'L11', items: [
+                {label: 'L11', value: 'L11'},
+                {label: 'L12', value: 'L12'},
+                {label: 'L13', value: 'L13'},
+                {label: 'L14', value: 'L14'},
+                {label: 'L15', value: 'L15'},
+            ]},
+            {label: 'L2', value: 'L11', items: [
+                {label: 'L21', value: 'L21'},
+                {label: 'L22', value: 'L22'},
+                {label: 'L23', value: 'L23'},
+                {label: 'L24', value: 'L24'},
+                {label: 'L25', value: 'L25'},
+            ]}];
+
+            this.theMenu.show(e);
+        }, 1000)
+        
+    }
+    //
+    menuItemClick(contextMenu: WjMenu) {
+        alert('thanks for selecting ' + contextMenu.selectedValue);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { WjNavModule } from '@grapecity/wijmo.angular2.nav';
+import { WjInputModule, WjMenu } from '@grapecity/wijmo.angular2.input';
+import { DataService } from './app.data';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [WjNavModule, WjInputModule, BrowserModule],
+    declarations: [AppComponent],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
